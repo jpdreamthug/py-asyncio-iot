22f1d29e12b7ab32226d2f1515ca3c2011169ae1
jpdreamthug <yaroslavkondrattsev@gmail.com>
2024-08-21 16:29:45 +0300

    add solution

--- app/iot/service.py
+++ app/iot/service.py
@@ -1,3 +1,4 @@
+import asyncio
 import random
 import string
 from typing import Protocol
@@ -13,21 +14,21 @@ def generate_id(length: int = 8) -> str:
 # so devices should not inherit for it (if it walks like a duck, and quacks like a duck, it's a duck)
 class Device(Protocol):
     def connect(self) -> None:
-        ...  # Ellipsis - similar to "pass", but sometimes has different meaning
+        pass  # Ellipsis - similar to "pass", but sometimes has different meaning
 
     def disconnect(self) -> None:
-        ...
+        pass
 
     def send_message(self, message_type: MessageType, data: str) -> None:
-        ...
+        pass
 
 
 class IOTService:
     def __init__(self) -> None:
         self.devices: dict[str, Device] = {}
 
-    def register_device(self, device: Device) -> str:
-        device.connect()
+    async def register_device(self, device: Device) -> str:
+        await device.connect()
         device_id = generate_id()
         self.devices[device_id] = device
         return device_id
@@ -39,11 +40,11 @@ class IOTService:
     def get_device(self, device_id: str) -> Device:
         return self.devices[device_id]
 
-    def run_program(self, program: list[Message]) -> None:
+    async def run_program(self, program: list[Message]) -> None:
         print("=====RUNNING PROGRAM======")
-        for msg in program:
-            self.send_msg(msg)
+        tasks = [self.send_msg(msg) for msg in program]
+        await asyncio.gather(*tasks)
         print("=====END OF PROGRAM======")
 
-    def send_msg(self, msg: Message) -> None:
-        self.devices[msg.device_id].send_message(msg.msg_type, msg.data)
+    async def send_msg(self, msg: Message) -> None:
+        await self.devices[msg.device_id].send_message(msg.msg_type, msg.data)
